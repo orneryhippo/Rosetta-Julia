c0cc9576f8b4117d9185b0eb146d66685b3afb6c
orneryhippo <jesse.cassill@gmail.com>
2019-01-30 14:43:23 -0800

    Update zebrapuzzle.jl

--- src/zebrapuzzle.jl
+++ src/zebrapuzzle.jl
@@ -1,27 +1,31 @@
 using Combinatorics 
 
-function make(str, test = (_) -> true)
+function make(str, test)  ### = (_) -> true)
    filter(test, collect(permutations(split(str))))
 end
- 
+
+function isleftof(color1, color2, hlist)
+   1 == findfirst(x -> x == color1, hlist) - findfirst(x -> x == color2, hlist)
+end
+
 men = make("danish english german norwegian swedish",
            x -> "norwegian" == x[1])
  
 drinks = make("beer coffee milk tea water", x -> "milk" == x[3])
  
 colors = make("blue green red white yellow",
-              x -> 1 == findfirst(x, "white") - findfirst(x, "green"))
+              x -> 1 == findfirst(c -> c == "white", x) - findfirst(c -> c == "green",x))
  
 pets = make("birds cats dog horse zebra")
  
 smokes = make("blend blue-master dunhill pall-mall prince")
  
 function eq(x, xs, y, ys)
-  findfirst(xs, x) == findfirst(ys, y)
+  findfirst(c -> c == x, xs) == findfirst(c -> c == y, ys)
 end
  
 function adj(x, xs, y, ys)
-  1 == abs(findfirst(xs, x) - findfirst(ys, y))
+  1 == abs(findfirst(c -> c == x, xs) - findfirst(c-> c == y, ys))
 end
  
 for m = men, c = colors
@@ -37,11 +41,12 @@ for m = men, c = colors
                  eq("swedish",m,"dog",p) &&
                  adj("blend",s,"cats",p) &&
                  adj("horse",p,"dunhill",s)
-                println("Zebra is owned by ", m[findfirst(p,"zebra")])
+                println("Zebra is owned by ", m[findfirst(c -> c == "zebra", p)])
                 println("Houses:")
-                for line = mapslices(xs -> join( map(s->rpad(s,12), xs)),
-                                     [p m c d s], 2)
-                  println(line)
+                for i in 1:5
+                  println("House $i")
+                  println(p[i]," ", m[i]," ", s[i]," ", c[i]," ", d[i])
+                  println()       
                 end
               end
             end

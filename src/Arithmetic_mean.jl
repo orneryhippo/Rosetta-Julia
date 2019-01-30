f48b1a20878a2221e751846507e7985adde462ef
orneryhippo <jesse.cassill@gmail.com>
2019-01-30 12:22:23 -0800

    Update Arithmetic_mean.jl

--- src/Arithmetic_mean.jl
+++ src/Arithmetic_mean.jl
@@ -1,7 +1,8 @@
 #Arithmetic mean
 #http://rosettacode.org/wiki/Averages/Arithmetic_mean
 
-# Julia's built-in mean function accepts AbstractArrays (vector, matrix, etc.)
+# Julia's <strike>built-in</strike> Statistics module mean function accepts AbstractArrays (vector, matrix, etc.)
+julia> using Statistics
 julia> mean([1,2,3])
 2.0
 julia> mean(1:10)
